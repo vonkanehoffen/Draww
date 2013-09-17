a2a12cc1eb0ac976a9eea857fe6b4c842cb4e6cb
Kane Clover <kane@assure24.com>
2013-09-17 18:54:46 +0100

    username field added (but still isn't used for auth). See: http://blog.12spokes.com/web-design-development/adding-custom-fields-to-your-devise-user-model-in-rails-4/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917172522) do
+ActiveRecord::Schema.define(version: 20130917173005) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20130917172522) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
