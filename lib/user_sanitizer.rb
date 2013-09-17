4cf753259a4c6fc3f401670c0d26ab6198cdf714
Kane Clover <kane@assure24.com>
2013-09-17 19:38:27 +0100

    user username field as auth instead of email

--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,15 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
+
+    def sign_in
+      default_params.permit(auth_keys)
+    end
+
+    def sign_up
+      default_params.permit(auth_keys + [:email, :password, :password_confirmation])
+    end
+
     def account_update
-        default_params.permit(:username, :email, :password, :password_confirmation, :current_password)
+        default_params.permit(auth_keys + [:email, :password, :password_confirmation, :current_password])
     end
 end
