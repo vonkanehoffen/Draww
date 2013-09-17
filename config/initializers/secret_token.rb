53e1ba1ab1c8137c225e68df96420796af397859
Kane Clover <kane@assure24.com>
2013-09-17 15:15:20 +0100

    Rails 4 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrawwV2::Application.config.secret_key_base = '569fd23c2acf040a8382284021a8608086fb234c2545427ba5a115dff10f26cef7fa3b13f356c70f4f6420a0f13ad191a26bb353ca34c17aea7b4b501ea98330'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrawwV2::Application.config.session_store :cookie_store, key: '_Draww_v2_session'
