8ebb32a0911a60613d96ce36d31a70db241aa192
Kane Clover <kane@assure24.com>
2013-09-18 08:34:51 +0100

    simplify posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,28 +27,20 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = current_user
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.' 
+    else
+      render action: 'new'
     end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
@@ -56,23 +48,16 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+    redirect_to posts_url
   end
 
   def vote_up
     vote = current_user.vote_up(@post)
-    respond_to do |format|
-      if vote.valid?
-        vote.save
-        format.html { redirect_to @post, notice: 'You voted.' }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to @post, notice: vote.errors.messages }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if vote.valid?
+      vote.save
+      redirect_to @post, notice: 'You voted.'
+    else
+      redirect_to @post, notice: vote.errors.messages
     end
   end
 
