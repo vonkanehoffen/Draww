e3f7766e867eb5bcc1f50c9953baa16f380fb0c2
Kane Clover <kane@assure24.com>
2013-10-07 19:55:52 +0100

    validation message on vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :user
-	validates_uniqueness_of :post_id, :scope => :user_id
+	validates_uniqueness_of :post_id, :scope => :user_id, :message => "You've already voted on this!"
 end
