33fb3ee75fa492944225ca0059710a2ef0f30e42
Kane Clover <kane@assure24.com>
2013-10-21 12:32:14 +0100

    use actual devise registration form on home page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+
+	# These 3 are necessary for using devise views on this controller.
+	def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
