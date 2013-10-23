cba3274fcfe4b3b52c0c720fbdc558d99a40c08a
Kane Clover <kane@assure24.com>
2013-10-23 20:26:20 +0100

    footer markup

--- app/assets/javascripts/general_ui.js
+++ app/assets/javascripts/general_ui.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
+
 	// Full screen action
 	$(document).on('click', '.post-fullscreen', function() {
 		console.log('fullscreen');
 		$(this).parents('.modal-dialog').width('100%');
 	})
 
+	// Update vote count 
     $('.vote-link').on('ajax:success', function(evt, data, status, xhr) {
     	console.log("ajax success");
         //console.info(evt);
         $(evt.target).parents('.post-show').find('.vote-count').text(data.post.vote_count);
     });
+
+    // Close signup nagging when button clicked
+	$('#close-signup-hero').click(function(){
+		$('#signup-hero').remove();
+	})
 })
