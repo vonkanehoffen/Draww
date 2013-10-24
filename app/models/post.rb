574f42d4232f302073eb5b56d3dc1d7612eee1fe
Richard Garbutt <richard@1000mileweb.com>
2013-10-24 20:07:18 +0100

    The new hotness

--- app/models/post.rb
+++ app/models/post.rb
@@ -37,16 +37,9 @@ class Post < ActiveRecord::Base
     self.save!
 	end
 
-  def self.hot(limit=20, offset=0)
-    self.find_by_sql <<eos
-      SELECT *, LOG10(ABS(vote_count) + 1) * SIGN(vote_count)   
-          + (UNIX_TIMESTAMP(created_at) / 300000)
-          as hotness
-      FROM posts
-      ORDER BY hotness
-           DESC
-      LIMIT #{offset}, #{limit}
-eos
+  def self.hot
+    hotness_field = "LOG10(ABS(vote_count) + 1) * SIGN(vote_count)   + (UNIX_TIMESTAMP(created_at) / 300000)"
+    select("*, #{hotness_field} as hotness").order('hotness')
   end
 
   private
