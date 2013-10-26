18e00319a03a890a7bc11f7645bc10c202aaf016
Kane Clover <kane@assure24.com>
2013-10-26 10:56:22 +0100

    default kaminari pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.hot
+    @posts = Post.hot.page(params[:page]).per(10)
   end
 
   # GET /posts/1
