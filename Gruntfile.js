05de3636c755beaa8339c6e2ccda1570bd14ac7c
Kane Clover <kane@assure24.com>
2013-10-27 14:00:15 +0000

    rearrange SVG assets and set up Grunticon for them

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,37 @@ module.exports = function (grunt) {
   "use strict";
   // Configuration
   grunt.initConfig({
-    "less": {
-      "main": {
+    less: {
+      main: {
         files: {
-          "app/assets/stylesheets/draww.css": "app/less/draww.less"
-        },
-        options: {
-          //yuicompress: true
+          "app/assets/stylesheets/draww.css": "app/grunt_assets/less/draww.less"
         }
       }
     },
-    "watch": {
-      "main": {
-        "files": ["app/less/draww.less"],
-        "tasks": ["less:main"]
+    grunticon: {
+      icons: {
+        options: {
+          src: "app/grunt_assets/svg/",
+          dest: "app/assets/grunticon",
+          datasvgcss: "../stylesheets/icons.data.svg.css",
+          customselectors: {
+            "cc_by_nc": ".cc",
+            "draww_logo": ".logo",
+            "spinner": ".spinner"
+          }
+        }
+      }
+    },
+    watch: {
+      main: {
+        files: ["app/grunt_assets/less/draww.less"],
+        tasks: ["less:main"]
       }
     }
   });
   // Task Loading
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('build', ["less:main"]);
+  grunt.registerTask('build', ["less:main", "grunticon:icons"]);
 };
