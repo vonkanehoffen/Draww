8bbb7783cc4f6e6057c8fc5efca8a5b232b27cab
Kane Clover <kane@assure24.com>
2013-10-27 15:52:00 +0000

    ajax post pagination spinner

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -11,14 +11,17 @@ $(function(){
     // On posts/index
     $(document).on('ajax:success', '.vote', function(evt, data, status, xhr) {
     	$(evt.target).text(data.post.vote_count);
-    })
+    });
 
     // Pagination /////////////////////////////////////////////////////////////
 
     $(document).on('ajax:success', '.load-more a', function(evt, data, status, xhr) {
     	$(evt.target).parents('.load-more').before($(data).find('.post'));
     	$(evt.target).parents('.load-more').replaceWith($(data).find('.load-more'));
-    })
+    });
+    $(document).on('ajax:send', '.load-more a', function(evt, data, status, xhr) {
+        $(evt.target).html('<div class="spinner-grey"></div>');
+    });
 
     // Spinners ///////////////////////////////////////////////////////////////
 
