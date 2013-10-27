e7bf41faace1ce87612485af6094d2d1ff2dfd3c
Kane Clover <kane@assure24.com>
2013-10-27 13:32:30 +0000

    reindent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
 module.exports = function (grunt) {
-    "use strict";
-    // Configuration
-    grunt.initConfig({
-        "less": {
-            "main": {
-                files: {
-                    "app/assets/stylesheets/draww.css": "app/less/draww.less"
-                },
-                options: {
-                    //yuicompress: true
-                }
-            }
+  "use strict";
+  // Configuration
+  grunt.initConfig({
+    "less": {
+      "main": {
+        files: {
+          "app/assets/stylesheets/draww.css": "app/less/draww.less"
         },
-        "watch": {
-            "main": {
-                "files": ["app/less/draww.less"],
-                "tasks": ["less:main"]
-            }
+        options: {
+          //yuicompress: true
         }
-    });
-    // Task Loading
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('build', ["less:main"]);
+      }
+    },
+    "watch": {
+      "main": {
+        "files": ["app/less/draww.less"],
+        "tasks": ["less:main"]
+      }
+    }
+  });
+  // Task Loading
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('build', ["less:main"]);
 };
