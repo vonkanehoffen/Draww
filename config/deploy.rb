4adda016a2e29f6651691c09d78324ef605f3ced
Kane Clover <kane@assure24.com>
2013-11-09 16:55:11 +0000

    add unicorn restart to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ namespace :deploy do
 
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute "/etc/init.d/unicorn_draww", "restart"
     end
   end
 
