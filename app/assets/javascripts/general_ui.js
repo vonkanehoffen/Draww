204d1e36775c8083c4d074c73ea0e7506c8ee8f5
Kane Clover <kane@assure24.com>
2013-11-10 17:30:23 +0000

    back button closes modal. close #40 #44

--- app/assets/javascripts/general_ui.js
+++ app/assets/javascripts/general_ui.js
@@ -24,4 +24,42 @@ $(function(){
 		placement: 'left'
 	});
 
+	// Make Back button close modal ///////////////////////////////////////////
+
+	var current_modal = '';
+
+	$(document).on('shown.bs.modal', '.modal', function() {
+		current_modal = $(this).attr('id');
+		window.location.hash = '#show-'+current_modal;
+	});
+	$(document).on('hidden.bs.modal', '.modal', function() {
+		removeHash();
+		window.location.hash = current_modal = '';
+	});
+	$(window).on('hashchange', function(e) {
+		if(window.location.hash != '#show-'+current_modal) {
+			$('#'+current_modal).modal('hide');
+		}
+		e.preventDefault();
+	});
 })
+
+// Helpers ////////////////////////////////////////////////////////////////////
+
+// See: http://stackoverflow.com/questions/1397329/how-to-remove-the-hash-from-window-location-with-javascript-without-page-refresh
+function removeHash () { 
+    var scrollV, scrollH, loc = window.location;
+    if ("pushState" in history)
+        history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = "";
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+}
